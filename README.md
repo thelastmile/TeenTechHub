993690f60791d7aea4cd6a5c146e2195cf00aa6c
hschoenburg <hschoenburg@gmail.com>
2015-11-15 15:20:05 -0800

    where da code at!

--- README.md
+++
@@ -1,5 +0,0 @@
-Each of us is responsible for different areas, so what we're actually doing is gathering content and grouping them together. 
-
-We have two things going right now, first a game, which starts in html/game.html and then the homepage, which is index.html.
-
-We have several sections of the homepage left to finish. And we need images.
---
+++ new_game/clues.js
@@ -0,0 +1,33 @@
+$(function() {
+	
+	// clues array contains object indicating where clues
+	// are located and what information they contain
+	var clues = [
+		{ 
+			image: '<img class="bcg" src="../images/game1.jpg" />',
+			top: 105,
+			problem: 'Create a variable called myVar and assign it the value \'my first variable\'',
+			solution: 'varmyVar=\"myfirstvariable\"',
+			clues: [
+				{ id: 'clue1', left: -145, top: null, clue: "Good job...you found your first clue. To understand and use Javascript, you must first understand variables. So, throughout level 1, you will be learning about variables." },
+				{ id: 'clue2', left: -600, top: null, clue: "Variables are like buckets of water, except they hold information, not water." },
+				{ id: 'clue3', left: -900, top: null, clue: "Variables are declared and assigned values." },		
+				{ id: 'clue4', left: -1420, top: null, clue: "To declare a variable and assign it a value, just type: <p>var anyNameIChoose = 'some value';" }
+			]
+		},  
+		{
+			image: '<img class="bcg" src="../images/game2.jpg" />',
+			top: 92,
+			problem: 'not yet',
+			solution: '',
+			clues: [
+				{ id: 'clue1', left: -160, top: null, clue: "Good job...you found your first clue. To understand and use Javascript, you must first understand variables. So, throughout level 1, you will be learning about variables." },
+				{ id: 'clue2', left: -640, top: null, clue: "Variables are like buckets of water, except they hold information, not water." },
+				{ id: 'clue2', left: -1515, top: null, clue: "Javascript variables are like buckets that hold information." },		
+				{ id: 'clue2', left: -1874, top: null, clue: "Javascript variables are like buckets that hold information." }
+			]
+		}
+	]
+
+	
+})
